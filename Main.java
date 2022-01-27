326372bce66c36e4662b9c0123e594cc6f1aa8ed
Ed Harcourt <edharcourt@gmail.com>
2022-01-27 10:15:09 -0500

    .

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Util.Weather;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+	    System.out.println(Util.Weather.f2c(211.9));
+    }
+}
